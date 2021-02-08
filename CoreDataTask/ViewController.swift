bbf460bb74dbf7f3f84c824670f57e8ed54a749f
Shanmugarajeshwaran R <shanmugarajeshwaranr@gmail.com>
2021-02-08 18:50:57 +0530

    Initial Commit

---
+++ CoreDataTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataTask
+//
+//  Created by ADMIN on 08/02/21.
+//  Copyright © 2021 Success Resource Pte Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
